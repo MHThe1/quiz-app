778cd96aea26a6fc144817d28401186d0f2b3561
Mehedi Hasan <mehedihtanvir@gmail.com>
2024-09-05 15:34:51 +0600

    Modern Family Added

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -14,6 +14,10 @@ import theOfficePic1 from "../assets/theOfficePic1.jpg";
 import theOfficePic2 from "../assets/theOfficePic2.jpg";
 import theOfficePic3 from "../assets/theOfficePic3.jpg";
 
+import mfPic1 from "../assets/mfPic1.jpg";
+import mfPic2 from "../assets/mfPic2.jpg";
+import mfPic3 from "../assets/mfPic3.jpg";
+
 import bbtPic1 from "../assets/bbtPic1.jpg";
 import bbtPic2 from "../assets/bbtPic2.jpg";
 import bbtPic3 from "../assets/bbtPic3.jpg";
@@ -38,6 +42,11 @@ const Categories = {
         name: "The Office",
         images: [theOfficePic1, theOfficePic2, theOfficePic3],
         alt: "The Office",
+    },
+    modernFamily: {
+        name: "Modern Family",
+        images: [mfPic1, mfPic2, mfPic3],
+        alt: "Modern Family",
     },
     bigBangTheory: {
         name: "The Big Bang Theory",
